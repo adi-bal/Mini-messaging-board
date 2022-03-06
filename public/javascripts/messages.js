ccd9b93a882c56ad23ce47ff08111f185ab55c8c
adi-bal <adi.ballaki@gmail.com>
2022-03-07 01:44:06 +0800

    added message support

---
+++ public/javascripts/messages.js
@@ -0,0 +1,23 @@
+const moment = require('moment');
+let date = moment().startOf('day').fromNow();
+let date2 = moment().startOf('hour').fromNow();
+
+// default messages shown in index
+const messages = [
+  {
+    title: 'new message',
+    description:
+      'message 1',
+    username: 'Haresh',
+    date: date2,
+  },
+  {
+    title: 'older message',
+    description:
+      'older message',
+    username: 'aditya',
+    date: date,
+  },
+];
+
+module.exports = messages;
--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,7 @@
 var express = require('express');
 var router = express.Router();
-
-const messages = [
-  {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date()
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date()
-  }
-];
+const moment = require('moment');
+const messages = require('../public/javascripts/messages');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
