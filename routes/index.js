f85b96a42e66a6ca1b42ebfbd3eabd34d5230e9c
adi-bal <adi.ballaki@gmail.com>
2022-03-07 00:48:16 +0800

    created form

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Message Board', messages: messages });
+});
+
+router.post('/new', (req, res, next) => {
+  const newMessage = {
+    title: req.body.title,
+    description: req.body.description,
+    username: req.body.username,
+    date: moment()
+      .startOf('hour' - 1)
+      .fromNow(),
+  };
+  messages.unshift(newMessage);
+  res.redirect('/');
 });
 
 module.exports = router;
